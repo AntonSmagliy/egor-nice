878692f6642eba6086588189176a8cbd02d8e405
Mazharenko <mazhor02@gmail.com>
2018-10-17 22:44:50 +0300

    night22332435555555544324324323423421231

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.sliders').slick({
-  	prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-	nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+  	prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+	nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>'
   });
 });
 	
