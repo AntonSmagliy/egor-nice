a2c3e9c339eeee02f15086be3e17aa7fb3af3881
Mazharenko <mazhor02@gmail.com>
2018-10-18 17:15:50 +0300

    night22332435555555544324324323423421231545441121

--- js/common.js
+++ js/common.js
@@ -24,4 +24,26 @@ $(document).ready(function(){
   	$('.of_wrap').toggleClass('ovrlhdn');
   });
 
+$("nav ul li").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    if($('body').width() <= 700){
+      $('nav').toggleClass('dbbl');
+    $('#overlay').toggleClass('dbbl');
+    $('body').toggleClass('ovrlhdn');
+    $('.of_wrap').toggleClass('ovrlhdn');
+    }
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+$('.top_arrow a').click(function() {
+    $('html, body').animate({scrollTop: 0},500);
+    return false;
+  })
 });
